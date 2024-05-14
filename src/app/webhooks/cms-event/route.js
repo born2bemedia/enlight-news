ceb1602adb7bfb6b35c3be35e4bc0daeb11ee2a3
SergWebAcceptance <serg.web@acceptance.ltd>
2024-05-14 23:32:39 +0300

    Fixed wehook

--- src/app/webhooks/cms-event/route.js
+++ src/app/webhooks/cms-event/route.js
@@ -3,8 +3,6 @@ import { revalidateTag } from "next/cache";
 
 export async function POST(request) {
   const payload = await request.json();
-  if (payload.model === "post") {
-    revalidateTag(CACHE_TAG_REVIEWS);
-  }
+  revalidateTag(CACHE_TAG_REVIEWS);
   return new Response(null, { status: 204 });
 }
